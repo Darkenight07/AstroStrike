a45aac33941db849232290255e0c486b99cc41e3
Darkenight07 <66468335+Darkenight07@users.noreply.github.com>
2023-11-29 15:20:16 +0100

    Nueva clase

---
+++ src/dev/darkenight07/Avion/BalasAvion.java
@@ -0,0 +1,22 @@
+package dev.darkenight07.Avion;
+
+public class BalasAvion {
+
+    public boolean haSidoDisparada = false;
+    public int x;
+    public int y;
+    public final int VELOCIDAD;
+
+    public BalasAvion(int x, int y, int velocidad) {
+        this.x = x;
+        this.y = y;
+        VELOCIDAD = velocidad;
+    }
+
+    public int getY() {
+        return y;
+    }
+    public int getX() {
+        return x;
+    }
+}
