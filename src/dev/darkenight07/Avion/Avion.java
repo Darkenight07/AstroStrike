80ff92780a211434784bbcf35049ce62f877fa83
Darkenight07 <66468335+Darkenight07@users.noreply.github.com>
2023-11-29 15:19:51 +0100

    Implementacion balas avion

--- src/dev/darkenight07/Avion/Avion.java
+++ src/dev/darkenight07/Avion/Avion.java
@@ -7,6 +7,8 @@ public class Avion {
     public int y;
     private final int VELOCIDAD;
     private KeyBoardListener keyBoardListener;
+    public BalasAvion balaAvion1;
+    public BalasAvion balaAvion2;
 
 
     public Avion(int x, int y, int velocidad) {
@@ -14,28 +16,41 @@ public class Avion {
         this.y = y;
         this.VELOCIDAD = velocidad;
         keyBoardListener = new KeyBoardListener();
+        balaAvion1 = new BalasAvion(340, 440, 2);
+        balaAvion2 = new BalasAvion(416, 440, 2);
     }
 
     public void mover() {
         if (keyBoardListener.estaSubiendo()) {
             y -= VELOCIDAD;
+            balaAvion1.y -= balaAvion1.VELOCIDAD;
+            balaAvion2.y -= balaAvion2.VELOCIDAD;
         }
         if (keyBoardListener.estaBajando()){
             y += VELOCIDAD;
+            balaAvion1.y += balaAvion1.VELOCIDAD;
+            balaAvion2.y += balaAvion2.VELOCIDAD;
         }
         if (keyBoardListener.estaIzquierda()) {
             x -= VELOCIDAD;
+            balaAvion1.x -= balaAvion1.VELOCIDAD;
+            balaAvion2.x -= balaAvion2.VELOCIDAD;
         }
         if (keyBoardListener.estaDerecha()) {
             x += VELOCIDAD;
-        }
-        if (keyBoardListener.estaDisparando()) {
-            disparar();
+            balaAvion1.x += balaAvion1.VELOCIDAD;
+            balaAvion2.x += balaAvion2.VELOCIDAD;
         }
     }
 
-    public void disparar() {
-        System.out.println("Disparando");
+    public boolean disparando() {
+        if (keyBoardListener.estaDisparando()) {
+            balaAvion1.haSidoDisparada = true;
+            balaAvion2.haSidoDisparada = true;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public int getX() {
@@ -43,6 +58,53 @@ public class Avion {
     }
     public int getY() {
         return y;
+    }
+
+    public int getBalasY(int opcionBala) {
+        if (opcionBala == 1) { // 1 = balaAvion1, 2 = balaAvion2
+            return balaAvion1.getY();
+        } else if  (opcionBala == 2) {
+            return balaAvion2.getY();
+        } else {
+            return 0;
+        }
+    }
+    public int getBalasX(int opcionBala) {
+        if (opcionBala == 1) { // 1 = balaAvion1, 2 = balaAvion2
+            return balaAvion1.getX();
+        } else if  (opcionBala == 2) {
+            return balaAvion2.getX();
+        } else {
+            return 0;
+        }
+    }
+
+
+    public void setBalasY(int opcionBala, int y) {
+        if (opcionBala == 1) { // 1 = balaAvion1, 2 = balaAvion2
+            balaAvion1.y = y;
+        } else if  (opcionBala == 2) {
+            balaAvion2.y = y;
+        }
+    }
+    public void setBalasX(int opcionBala, int x) {
+        if (opcionBala == 1) { // 1 = balaAvion1, 2 = balaAvion2
+            balaAvion1.x = x;
+        } else if  (opcionBala == 2) {
+            balaAvion2.x = x;
+        }
+    }
+
+    public void disparoBalas() {
+        balaAvion1.y -= balaAvion1.VELOCIDAD;
+        balaAvion2.y -= balaAvion2.VELOCIDAD;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
     }
 
     public KeyListener getKeyBoardListener() {
